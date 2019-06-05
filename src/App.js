067cbe63c1f2fa8116444ed03f614580fe01f158
Kurt Peek <kurt.peek@gmail.com>
2019-06-05 14:51:55 -0700

    Added Material UI Container

--- src/App.js
+++ src/App.js
@@ -3,28 +3,33 @@ import './App.css';
 import Map from './components/Map';
 import GroundOverlay from './components/GroundOverlay';
 import SearchBox from './components/SearchBox';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
 
 function App() {
   const [bounds, setBounds] = useState({});
 
   return (
     <div className="App">
-      <Map
-        zoom={12}
-        center={{ lat: 40.740, lng: -74.18 }}
-        events={{ onBoundsChanged: arg => setBounds(arg) }}
-      >
-        <SearchBox />
-        <GroundOverlay
-          url='https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg'
-          bounds={{
-            north: 40.773941,
-            south: 40.712216,
-            east: -74.12544,
-            west: -74.22655
-          }}
-          opacity={0.5}/>
-      </Map>
+      <CssBaseline />
+      <Container>
+        <Map
+          zoom={12}
+          center={{ lat: 40.740, lng: -74.18 }}
+          events={{ onBoundsChanged: arg => setBounds(arg) }}
+        >
+          <SearchBox />
+          <GroundOverlay
+            url='https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg'
+            bounds={{
+              north: 40.773941,
+              south: 40.712216,
+              east: -74.12544,
+              west: -74.22655
+            }}
+            opacity={0.5}/>
+        </Map>
+      </Container>
     </div>
   );
 }
