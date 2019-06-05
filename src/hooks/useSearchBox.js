0d027ed7d2eb654b7ef0888620403253e94a7805
Kurt Peek <kurt.peek@gmail.com>
2019-06-05 14:42:49 -0700

    Made SearchBox update map location

--- src/hooks/useSearchBox.js
+++ src/hooks/useSearchBox.js
@@ -12,6 +12,24 @@ export default function useSearchBox({ maps, map }) {
       searchBox.setBounds(map.getBounds());
     })
 
+    searchBox.addListener('places_changed', function() {
+      const places = searchBox.getPlaces();
+
+      if (!places) {
+        return;
+      }
+
+      let bounds = new maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (place.geometry.viewport) {
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    })
+
     setSearchBox(searchBox);
   }, [])
   return { searchBox, searchBoxRef }
